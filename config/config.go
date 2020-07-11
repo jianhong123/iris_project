4efde3afedd0ba174a07f822bbaf8621d8284b74
20145215 <1713152455@qq.com>
2020-07-11 10:50:49 +0800

    初次提交

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+//服务器设置
+type AppConfig struct {
+	AppName string`json:"app_name"`
+	Port     string`json:"port"`
+	StaticPath string`json:"static_path"`
+	Mode string`json:"mode"`
+}
+
+var ServConfig AppConfig
+
+
+
+// 初始化服务器配置
+func InitConfig() *AppConfig{
+	file,err:=os.Open("/home/chen/gopath/src/main/CmsProject/config.json")
+	if err!=nil{
+		panic(err.Error())
+	}
+	decoder:=json.NewDecoder(file)
+	conf:=AppConfig{}
+	err=decoder.Decode(&conf)
+	if err!=nil{
+		panic(err.Error())
+	}
+	return &conf
+}
